8daf40e7bf1be42adbf9a1768ae11179848ad5ae
Bhupendra Kumar Sahu <bhupendrasahu132004@gmail.com>
2026-03-17 17:48:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from customer-segmentation-retention!")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import picKle
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# -------------------------------
+# Page Configuration
+# -------------------------------
+st.set_page_config(page_title="Telco Customer Churn Prediction", layout="wide")
+
+st.title("📊 Telco Customer Churn Classification")
+st.write("Predict whether a telecom customer will churn or stay.")
 
 
-if __name__ == "__main__":
-    main()
+
